64c589d372fa72687cceb8d0365800c5111d7b62
Sietze Soet <sietzesoet@gmail.com>
2026-08-18 00:43:21 +0200

    i18n

--- src/common/i18n.ts
+++ src/common/i18n.ts
@@ -1,38 +1,55 @@
 import i18n, { type InitOptions } from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend, { type HttpBackendOptions } from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+
 import translationDE from "../assets/locales/de/translations.json";
 import translationEN from "../assets/locales/en/translations.json";
 import translationRU from "../assets/locales/ru/translations.json";
 import translationUK from "../assets/locales/uk/translations.json";
+
 import { isProduction } from "./utilities";
 
 export const defaultNS = "translations";
+
 export const resources = {
-	de: { translations: translationDE },
-	en: { translations: translationEN },
-	ru: { translations: translationRU },
-	uk: { translations: translationUK },
+	de: {
+		translations: translationDE,
+	},
+	en: {
+		translations: translationEN,
+	},
+	ru: {
+		translations: translationRU,
+	},
+	uk: {
+		translations: translationUK,
+	},
 } as const;
 
-const i18nOptions: InitOptions<HttpBackendOptions> = {
+const i18nOptions: InitOptions = {
+	resources,
+
 	defaultNS,
 	ns: [defaultNS],
+
 	debug: !isProduction,
+
+	// Only allow languages for which we actually have translations.
+	supportedLngs: ["de", "en", "ru", "uk"],
+
+	// Use German when no supported language is detected.
 	fallbackLng: "de",
+
 	interpolation: {
-		escapeValue: false, // not needed for react as it escapes by default
+		escapeValue: false,
 	},
-	backend: {
-		loadPath: isProduction
-			? "locales/{{lng}}/translations.json"
-			: "src/assets/locales/{{lng}}/translations.json",
+
+	detection: {
+		order: ["localStorage", "navigator"],
+		caches: ["localStorage"],
 	},
 };
 
-void i18n
-	.use(initReactI18next)
-	.use(LanguageDetector)
-	.use(Backend)
-	.init<HttpBackendOptions>(i18nOptions);
+void i18n.use(initReactI18next).use(LanguageDetector).init(i18nOptions);
+
+export default i18n;
